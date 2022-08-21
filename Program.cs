35a4e8dec99665d7bb54aab1c63c8510330c5bf2
olga <shkvyryaos@gmail.com>
2022-08-21 23:57:14 +0500

    furst commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿
+/*
+
+//Задача 1.  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Inrut furst integer number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inrut second integer number ");
+ int number2 = Convert.ToInt32(Console.ReadLine());
+if  (number1>number2)
+{
+    Console.WriteLine ($"max= {number1}");
+    Console.WriteLine ($"min= {number2}");
+}
+else {
+    Console.WriteLine ($"max= {number2}");
+    Console.WriteLine ($"min= {number1}");
+}
+*/
+
+
+
+/*
+//Задача 2.  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int max;
+Console.WriteLine("Inrut furst integer number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inrut second integer number ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inrut third integer number ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if  (number1>number2)
+{
+   max=number1;
+}
+else {
+    max=number2;
+}
+if (max>number3)
+{
+  Console.WriteLine ($"max= {max}");  
+}
+else
+{
+ max=number3;
+  Console.WriteLine ($"max= {max}");  
+}
+
+*/
+
+
+
+/*
+
+//Задача 3.  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine(" Ввведите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if  (number%2==0)
+{
+    Console.WriteLine ($"{number} - четное число");
+}
+else {
+    Console.WriteLine ($"{number} - нечетное число");
+}
+*/
+/*
+//Задача 4.  Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Inrut positive integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+int current=1;
+while (current<=number)
+    {
+        if  (current%2==0)
+{
+    Console.Write (current + " ");
+    }
+    current++;}
+*/
